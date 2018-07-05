e3d25cb9855ce2d8b23e2cb098d2bde0a02b81ec
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-07-05 16:40:46 +1000

    add webpack-cli and import module

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+// four core concepts: the entry point, the output, loaders, and plugins
+
+module.exports = {
+    //the entry point is where webpack starts bundling.
+    entry: './src/js/index.js',
+    // tell webpack where to save our bundle file
+    output: {
+    // use a method which is the path package - path.resolve
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'bundle.js'
+    },
+}
